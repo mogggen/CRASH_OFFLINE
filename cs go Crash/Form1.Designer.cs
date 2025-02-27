0db1a6f94fd6bf269fabd9cf3619629ced77acc7
Morgan Nyman <morgan.nyman@elecosoft.com>
2025-02-27 10:22:12 +0100

    Initial crash commit

---
+++ cs go Crash/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace cs_go_Crash
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(36, 24);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 100);
+			this.panel1.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.Red;
+			this.label4.Location = new System.Drawing.Point(61, 13);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(81, 20);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Crashed!";
+			this.label4.Visible = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.SpringGreen;
+			this.label1.Location = new System.Drawing.Point(55, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(87, 37);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "x1.00";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Black;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.Cyan;
+			this.label2.Location = new System.Drawing.Point(262, 37);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 17);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "#";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(36, 156);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(50, 26);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "bet";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 10;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(130, 130);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(106, 52);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "start";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Black;
+			this.label3.ForeColor = System.Drawing.Color.Cyan;
+			this.label3.Location = new System.Drawing.Point(262, 85);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(18, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "@";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(36, 130);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+			this.numericUpDown1.TabIndex = 7;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Click += new System.EventHandler(this.numericUpDown1_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.SpringGreen;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(92, 130);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(44, 23);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "MAX";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.SpringGreen;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(92, 159);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(44, 23);
+			this.button4.TabIndex = 9;
+			this.button4.Text = "50%";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(471, 204);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Crash Royale";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ cs go Crash/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs_go_Crash
+{
+	public partial class Form1 : Form
+	{
+		Random rand = new Random();
+
+		bool Crashing = false;
+		int coins = 100;
+		double multiplier = 1.00;
+		int bet = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+			button2.Text = "start";
+			update();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			multiplier += multiplier / 1000;
+			int lucky = rand.Next(1, (int)(multiplier + 1000));
+			if (lucky <= multiplier)
+			{
+				Crash();
+			}
+			update();
+		}
+
+		private void Crash()
+		{
+			timer1.Enabled = false;
+			label1.ForeColor = Color.Red;
+			bet = 0;
+			button2.Text = "start";
+			label4.Visible = true;
+			label4.Text = "Crashed!";
+			Crashing = false;
+			update();
+		}
+
+		private void update()
+		{
+			label1.Text = $"x{Math.Round(multiplier, 2)}";
+			label2.Text = $"Coin(s): {coins}";
+			label3.Text = $"{bet} * {Math.Round(multiplier, 2)} = {Math.Round(bet * multiplier)}";
+		}
+
+		private void start()
+		{
+			label4.Visible = false;
+			label1.ForeColor = Color.SpringGreen;
+			multiplier = 1;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (bet > 0)
+			{
+				if (!Crashing)
+				{
+					start();
+					button2.Text = "stop";
+					Crashing = true;
+					timer1.Enabled = true;
+				}
+
+				else
+				{
+					timer1.Enabled = false;
+					button2.Text = "start";
+					coins += (int)(Math.Round(bet * multiplier));
+					multiplier = 1;
+					bet = 0;
+					update();
+					Crashing = false;
+				}
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (coins >= int.Parse(numericUpDown1.Text))
+				{
+					start();
+					coins -= int.Parse(numericUpDown1.Text);
+					bet += int.Parse(numericUpDown1.Text);
+					update();
+				}
+			}
+			catch
+			{
+
+			}
+		}
+
+		private void numericUpDown1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			update();
+			numericUpDown1.Value = coins;
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			update();
+			numericUpDown1.Value = (int)Math.Round((double)coins / 2);
+		}
+	}
+}
